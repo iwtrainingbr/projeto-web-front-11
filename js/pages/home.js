67895467b9842d184a14516a9575e0618998a98e
unknown <bergnunes@gmail.com>
2021-11-26 21:11:20 -0300

    Atualizado a pagina home com usuario/perguntas/categorias/etc

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,8 +2,14 @@ function home() {
   return `
     ${navbar()}
     <hr>
-    <h1>Bem vindo</h1>
-    <hr>
+    <article class="row">
+      <div class="rounded bg-primary col-sm p-3"><p><strong>Usuários</strong></p><button class="btn btn-info disable mb-2">99</button></div>
+      &nbsp;<div class="rounded bg-success col-sm p-3"><p><strong>Perguntas</strong></p><button class="btn btn-info disable mb-2">20</button></div>
+      &nbsp;<div class="rounded bg-danger col-sm p-3"><p><strong>Categorias</strong></p><button class="btn btn-info disable mb-2">40</button></div>
+      &nbsp;<div class="rounded bg-warning col-sm p-3"><p><strong>Etc..</strong></p><button class="btn btn-info disable mb-2">50</button></div>
+    </article>
+
+    <br>
     ${footer()}
   `;
 }
