51eebf95a0942625bd37d09a3846b220ab56cc0d
TalisonMikael <talisonmikael@hotmail.com>
2021-12-02 21:07:24 -0300

    atualizei o cadastrar categoria

--- js/pages/cadastrarcategoria.js
+++ js/pages/cadastrarcategoria.js
@@ -1,23 +1,43 @@
+
 function cadastrarcategoria() {
     return `
       ${navbar()}
-      <section class="row">
-          <hr>
-              <h1 class="mt-5">Cadastrar Categoria</h1>
+      <style>
+        @font-face {
+            font-family: 'Love3';
+            src: url(fonts/love3.ttf) format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+        section {
+            font-family: 'Love3', 'Times New Roman', Times, serif;
+            background-image: linear-gradient(to right, #fff, #ceebffc4);
+            color: #2397e4e5;
+            border-radius: 10px;
+            box-shadow: 5px 5px 15px #616464a1;
+            width: 500px;
+            padding: 10px;
+            margin: auto;
+        }
+      </style>
+      <main>
+      <hr>
+          <section>
+                  <h1>Cadastrar Categoria</h1>
           
-          <form class="col-5" method="get" action="#">
-              <br>
-              <label for="nome">Nome</label>
-              <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
-              <br>
+              <form action="#">
+                  <br>
+                  <label for="nome">Nome</label>
+                  <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
+                  <br>
           
-              <label for="desc">Descrição</label>
-              <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
-              <br>
-
-              <button class="btn btn-primary">Enviar</button>
-          </form>
-      </section>
+                  <label for="desc">Descrição</label>
+                  <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
+                  <br>
+                  <button class="btn btn-primary">Enviar</button>
+              </form>
+          </section>
+      </main>
       ${footer()}
     `;
     
