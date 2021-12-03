baf5514a3510962307a4798d31267c28175a046b
TalisonMikael <talisonmikael@hotmail.com>
2021-12-02 21:44:19 -0300

    consertei

--- js/pages/cadastrarcategoria.js
+++ js/pages/cadastrarcategoria.js
@@ -21,68 +21,27 @@ function cadastrarcategoria() {
       </style>
       <main class="row">
       <hr>
-          <section class="col-6">
-                  <div>
-                      <h1>Cadastrar Categoria</h1>
-                                
-                                    <form action="#">
-                      <br>
-                      <label for="nome">Nome</label>
-                      <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
-                      <br>
-                      <label for="desc">Descrição</label>
-                      <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
-                      <br>
-                      <button class="btn btn-primary">Enviar</button>
-                                    </form>
-                  </div>
-          <section>
-                  <h1>Cadastrar Categoria</h1>
-          
-              <form action="#">
-                  <br>
-                  <label for="nome">Nome</label>
-                  <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
-                  <br>
-                  <label for="desc">Descrição</label>
-                  <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
-                  <br>
-                  <button class="btn btn-primary">Enviar</button>
-              </form>
-          </section>
-          <article class="col-6">
-              <div>
-                  <img src="img/musica.png">
-              </div>
-          </article>
-      </main>
-              <label for="desc">Descrição</label>
-              <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
-              <br>
-
-              <button class="btn btn-primary">Enviar</button>
-          </form>
-      </section>
-      <hr>
-          <h1>Cadastrar Categoria</h1>
-
-          <section class="row ">
-            <div class="col-md-6">
-            <article class="card card-body">
+         <section class="col-6">
+            <div>
+              <h1>Cadastrar Categoria</h1>
+                               
                 <form action="#">
-                    <br>
-                    <label for="nome">Nome:</label>
-                    <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
+                     <br>
+                    <label for="nome">Nome</label>
+                     <input class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
                     <br>
 
-                    <label for="desc">Descrição:</label>
+                    <label for="desc">Descrição</label>
                     <textarea class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
                     <br>
+
                     <button class="btn btn-primary">Enviar</button>
-                </form>
-              </article>
+                 </form>
             </div>
-          </section>
+         </section>
+         <article class="col-6">
+            <img src="img/musica.png">
+         </article>
       ${footer()}
     `;
 
