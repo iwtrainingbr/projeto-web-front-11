0c1507fca7b495fe9923457843005a6455de414a
Chagas Barros <francisco_publicidade@yahoo.com.br>
2021-12-05 13:08:08 -0300

    consertei o formulario e fiz a imagem aparecer

--- js/pages/cadastrarcategoria.js
+++ js/pages/cadastrarcategoria.js
@@ -25,23 +25,22 @@ function cadastrarcategoria() {
             font-weight: normal;
             font-style: normal;
         }
-        section {
-            font-family: 'Love3', 'Times New Roman', Times, serif;
-            background-image: linear-gradient(to right, #fff, #ceebffc4);
-            color: #2397e4e5;
-            border-radius: 10px;
-            box-shadow: 5px 5px 15px #616464a1;
-            width: 500px;
-            padding: 10px;
+
+        h2, label {
+          color: #06A4AE;
         }
+        img {
+          width: 500px;
+        }
+
       </style>
       <main class="row">
       <hr>
          <section class="col-6">
             <div>
-              <h1>Cadastrar Categoria</h1>
 
-                <form method="post" id="form_category" action="#" onsubmit="saveCategory()">
+                <form class="form-control" method="post" id="form_category" action="#" onsubmit="saveCategory()">
+                              <h2>Cadastrar Categoria</h2>
                      <br>
                     <label for="nome">Nome</label>
                      <input id="category_name" class="form-control" type="text" name="nome" placeholder="Ex: Musica" required>
@@ -51,12 +50,12 @@ function cadastrarcategoria() {
                     <textarea id="category_description" class="form-control" name="desc" id="descricao" placeholder="Digite aqui..."></textarea>
                     <br>
 
-                    <button class="btn btn-primary">Enviar</button>
+                    <button class="btn btn-info">Enviar</button>
                  </form>
             </div>
          </section>
          <article class="col-6">
-            <img src="img/musica.png">
+            <img src="img/ilustras/musica.png">
          </article>
       ${footer()}
     `;
