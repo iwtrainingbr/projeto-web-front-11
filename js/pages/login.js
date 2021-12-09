3d4584a33ce4476bc3ddb555eeb382805af3ded3
bergnunes <bergnunes@gmail.com>
2021-12-09 17:15:51 -0300

    Ajuste_de_espaçamento_login

--- js/pages/login.js
+++ js/pages/login.js
@@ -2,28 +2,27 @@ function login() {
     return `
     <form class="bg-white p-3 login" onsubmit="changeContent('login')">
 
-        <div><img src="img/we_match!02.png" class="mx-auto d-block mt-3 pt-3"></div>
+        <div><img src="img/we_match!02.png" class="mx-auto d-block mt-2 pt-2"></div>
 
         <h3 class="text-center mt-2">Fazer login</h4>
         <h6 class="text-center">Ir para o We Match!</h5><br>
 
         <div class="mb-3">
             <label for="#" class="form-label">Email</label>
-            <input type="email" class="form-control" id="#" placeholder="nome@examplo.com">
+            <input type="email" class="form-control" id="#" placeholder="nome@examplo.com" aria-label="email">
         </div>
 
         <div class="mb-3">
             <label for="senha" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="senha">
+            <input type="password" class="form-control" id="senha" placeholder="********">
         </div>
-        <button class="btn btn-info btn-lg float-end" onclick="changeContent('home')">Enviar</button><br><br>
+        <button class="btn btn-info btn-lg float-end" onclick="changeContent('home')">Enviar</button>
 
+        </form>
 
-        <div class="text-center">
+        <div class="text-center mt-3">
             <a class="text-center" href="#">Esqueceu a senha?</a><br>
             <a class="text-center" href="#">Clique aqui</a>
-        </div>
-
-    </form>
+        </div><br>
     `
 }
