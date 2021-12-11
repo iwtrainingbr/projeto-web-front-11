7cebb41f978c12a008e0b8194327856203e5d9d3
Leandroscrypt <leandrodes.goncalves@gmail.com>
2021-12-10 21:20:21 -0300

    Incluindo imagem avatar num modal

--- js/pages/listUsers.js
+++ js/pages/listUsers.js
@@ -1,11 +1,16 @@
-function listUsers(){
-  fetch (API_URL+'/users.json')
-  .then(response => response.json())
-  .then(users => {
-    for (let id in users) {
-      document.getElementById('table_users').innerHTML += `
+function updatePhoto(src) {
+    document.getElementById('userPhoto').src = src
+}
+
+function listUsers() {
+    fetch(API_URL + '/users.json')
+        .then(response => response.json())
+        .then(users => {
+            for (let id in users) {
+                document.getElementById('table_users').innerHTML += `
       <tr>
-        <td><img src="${users[id].photo}" width = '50'></td>
+        <td>
+        <img src="${users[id].photo}" onclick="${updatePhoto(users[id].photo)}" data-bs-toggle="modal" data-bs-target="#modalEditUser" width = '50'></td>
         <td>${users[id].name}</td>
         <td>${users[id].city}</td>
         <td>${users[id].status}</td>
@@ -15,15 +20,33 @@ function listUsers(){
           </td>
       </tr>
         `;
-      }
-  });
+            }
+        });
 
 
-return `
+    return `
   ${navbar()}
   <hr>
     <h1>Listar Usuários</h1>
   </hr>
+
+  <!-- Modal -->
+<div class="modal fade" id="modalEditUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Editar Perfil</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img height="100" width="100" id="userPhoto" width = '50'>
+      </div>
+      <div class="modal-footer">
+        <button id="modal-cancel" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
 
   <article class="card card-body">
     <table class="table table-hover table-striped">
